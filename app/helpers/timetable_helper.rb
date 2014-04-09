fe4daa5afaab75977f5b9eb19ba26f489ddea34c
Mikael Borg <mikael.borg@bils.se>
2014-04-09 20:32:01 +0000

    Issues per user works.

--- app/helpers/timetable_helper.rb
+++ app/helpers/timetable_helper.rb
@@ -1,7 +1,37 @@
-module TimetableHelper
-    include ApplicationHelper
+# encoding: utf-8
+#
+# Redmine - project management software
+# Copyright (C) 2006-2014  Jean-Philippe Lang
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-def donothing
- print " \n"
-end
+module TimetableHelper
+  def users_status_options_for_select(selected)
+    user_count_by_status = User.group('status').count.to_hash
+    options_for_select([[l(:label_all), ''],
+                        ["#{l(:status_active)} (#{user_count_by_status[1].to_i})", '1'],
+                        ["#{l(:status_registered)} (#{user_count_by_status[2].to_i})", '2'],
+                        ["#{l(:status_locked)} (#{user_count_by_status[3].to_i})", '3']], selected.to_s)
+  end
+
+  def issues_for_user(id)
+    Issue.visible.
+      where(:assigned_to_id => id).
+      includes(:status, :project, :tracker).
+      order("#{Issue.table_name}.updated_on DESC").
+      all
+  end
+
 end
