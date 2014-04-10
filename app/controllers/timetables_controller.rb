83bc2d6ac6a7b7053d92d2f27c9748e38bb985b2
Mikael Borg <mikael.borg@bils.se>
2014-04-10 21:32:25 +0000

    First timeline view visible.

--- app/controllers/timetables_controller.rb
+++ app/controllers/timetables_controller.rb
@@ -16,7 +16,7 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 class TimetablesController < ApplicationController
-  layout 'admin'
+  layout 'ttbase'
 
   before_filter :require_admin, :except => :show
   skip_before_filter :find_user
