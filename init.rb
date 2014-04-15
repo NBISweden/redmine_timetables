00ea20dd34be19b1cecb1f24c1f3ca6ce60285cb
borg <borg@localhost.localdomain>
2014-04-15 09:02:09 +0200

    Add github link.

--- init.rb
+++ init.rb
@@ -3,7 +3,7 @@ Redmine::Plugin.register :redmine_timetables do
   author 'Mikael Borg'
   description 'Timetables for Redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
+  url 'http://github.com/nmb-se/redmine_timetables'
   author_url 'http://bils.se/about'
   menu :admin_menu, :timetable, { :controller => 'timetables', :action => 'show' }, :caption => 'Timetable'
 end
