a7078a5a8618cc4cabdcd04cef6b8f471f1bcfdc
borg <borg@localhost.localdomain>
2014-04-15 08:58:41 +0200

    Add menu item to admin menu.

--- init.rb
+++ init.rb
@@ -5,4 +5,5 @@ Redmine::Plugin.register :redmine_timetables do
   version '0.0.1'
   url 'http://example.com/path/to/plugin'
   author_url 'http://bils.se/about'
+  menu :admin_menu, :timetable, { :controller => 'timetables', :action => 'show' }, :caption => 'Timetable'
 end
