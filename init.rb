94412f3464b110b3a90f18e4f67713959ef3d8b1
borg <borg@localhost.localdomain>
2014-12-08 13:18:18 +0100

    Improve docs. Update metadata.

--- init.rb
+++ init.rb
@@ -1,12 +1,13 @@
+require 'redmine'
+
 Redmine::Plugin.register :redmine_timetables do
   name 'Redmine Timetables plugin'
   author 'Mikael Borg'
-  description 'Timetables for Redmine'
-  version '0.0.1'
+  description 'Timetables for Redmine: adds views for user schedules.'
+  version '0.1'
   url 'http://github.com/nmb-se/redmine_timetables'
-  author_url 'http://bils.se/about'
   project_module :timetables do
-  permission :timetables, { :timetables => [:show] }, :public => true
+    permission :timetables, { :timetables => [:show] }, :public => true
   end
   menu :project_menu, :timetables, { :controller => 'timetables', :action => 'show' }, :caption => 'Timetable', :after => :activity, :param => :project_id
   menu :admin_menu, :timetable, { :controller => 'timetables', :action => 'show' }, :caption => 'Timetable'
